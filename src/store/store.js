d72f80fee6bd185f9820bc0b5317b622f6ee4649
Sebastian madero <rockmadero@gmail.com>
2022-11-29 13:31:10 -0500

    AuthSlice added

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { uiSlice, calendarSlice } from "./";
+import { uiSlice, calendarSlice, authSlice } from "./";
 
 export const store = configureStore({
-    // middleware: getDefaultMiddleware =>
-    //     getDefaultMiddleware({
-    //         serializableCheck: false,
-    // }),
+    middleware: getDefaultMiddleware =>
+        getDefaultMiddleware({
+            serializableCheck: false,
+    }),
     reducer:{
+        auth: authSlice.reducer,
         calendar: calendarSlice.reducer,
         ui: uiSlice.reducer,
     }
