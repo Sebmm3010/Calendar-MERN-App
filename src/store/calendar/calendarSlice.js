c06555c35e8c403adaac9d2f1edca376d14443dc
Sebastian madero <rockmadero@gmail.com>
2022-12-01 16:55:52 -0500

    Cargar y mostrar los eventos complete

--- src/store/calendar/calendarSlice.js
+++ src/store/calendar/calendarSlice.js
@@ -1,23 +1,26 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { addHours } from 'date-fns';
+// import { addHours } from 'date-fns';
 
-const tempEvent = {
-    _id: new Date().getTime(),
-    title: 'Cupleaños',
-    notes: 'Comprar pudin',
-    start: new Date(),
-    end: addHours(new Date(), 2),
-    bgColor: '#fafafa',
-    user: {
-        _id: '123',
-        name: 'Sebastian'
-    }
-}
+// const tempEvent = {
+//     _id: new Date().getTime(),
+//     title: 'Cupleaños',
+//     notes: 'Comprar pudin',
+//     start: new Date(),
+//     end: addHours(new Date(), 2),
+//     bgColor: '#fafafa',
+//     user: {
+//         _id: '123',
+//         name: 'Sebastian'
+//     }
+// }
 
 export const calendarSlice = createSlice({
     name: 'calendar',
     initialState: {
-        events: [tempEvent],
+        isLoadingEvents: true,
+        events: [
+            /* tempEvent */
+        ],
         activeEvent: null
     },
     reducers: {
@@ -37,16 +40,28 @@ export const calendarSlice = createSlice({
             });
         },
         onDeleteEvent: (state) => {
-            if(state.activeEvent){
-                state.events= state.events.filter(event=>{
+            if (state.activeEvent) {
+                state.events = state.events.filter(event => {
                     return event._id !== state.activeEvent._id
                 });
-                state.activeEvent=null;
+                state.activeEvent = null;
             }
+        },
+        onLoadEvents: (state, { payload = [] }) => {
+            state.isLoadingEvents = false;
+            // state.events=payload;
+            payload.forEach(event => {
+                const exist = state.events.some(dbEvent => dbEvent.id === event.id);
+
+                if (!exist) {
+                    state.events.push(event);
+                }
+            });
+
         }
     }
 });
 
 
 // Action creators are generated for each case reducer function
-export const { onSetActiveEvent, onAddNewEvent, onUpdateEvent, onDeleteEvent } = calendarSlice.actions;
+export const { onSetActiveEvent, onAddNewEvent, onUpdateEvent, onDeleteEvent, onLoadEvents } = calendarSlice.actions;
